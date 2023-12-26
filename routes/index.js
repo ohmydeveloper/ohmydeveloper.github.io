385730342affbc5c5aa6a39f276e16cf44d7d7e2
Kyrylo Panin <k.panindevelop@gmail.com>
2023-12-26 23:17:25 +0200

    Initial commit

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/links', function(req, res, next) {
-  res.render('links', { title: 'Links' });
-});
-
-module.exports = router;
